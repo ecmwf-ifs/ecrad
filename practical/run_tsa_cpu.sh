f6b769b56e2feb01e4ccd4705dcc81d4e4c80f52
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2021-07-27 17:15:48 +0200

    add cpu runscipt

---
+++ practical/run_tsa_cpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name="ecrad_cpu"
+#SBATCH --account="s83"
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=debug
+#SBATCH --constraint=gpu
+#SBATCH --exclusive
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export CRAY_CUDA_MPS=1
+
+srun ../bin/ecrad config.nam era5slice_2500.nc tt.nc
+# srun ../bin/ecrad config.nam era5slice_25000.nc tt.nc
+
+# # SBATCH --hint=nomultithread
