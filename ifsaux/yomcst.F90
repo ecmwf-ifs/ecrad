2e0aa9c46c52a54f0f92fcd91c9aeb20bb93e74b
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-03-28 09:22:02 +0000

    Initialize parameters in yomcst

--- ifsaux/yomcst.F90
+++ ifsaux/yomcst.F90
@@ -34,22 +34,20 @@ REAL(KIND=JPRB), PARAMETER :: RMO3 = 47.9942_JPRB
 ! * RI0          : solar constant
 REAL(KIND=JPRB), PARAMETER :: RI0 = 1366.0_JPRB
 ! * RMCO2        : CO2 (carbon dioxide) molar mass
-REAL(KIND=JPRB), PARAMETER :: RMCO2 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMCO2 = 44.0095_JPRB
 ! * RMCH4        : CH4 (methane) molar mass
-REAL(KIND=JPRB), PARAMETER :: RMCH4 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMCH4 = 16.04_JPRB
 ! * RMN2O        : N2O molar mass
-REAL(KIND=JPRB), PARAMETER :: RMN2O = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMN2O = 44.013_JPRB
 ! * RMNO2        : NO2 (nitrogen dioxide) molar mass
-REAL(KIND=JPRB), PARAMETER :: RMNO2 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMNO2 = 46.01_JPRB
 ! * RMCFC11      : CFC11 molar mass
-REAL(KIND=JPRB), PARAMETER :: RMCFC11 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMCFC11 = 137.3686_JPRB
 ! * RMCFC12      : CFC12 molar mass
-REAL(KIND=JPRB), PARAMETER :: RMCFC12 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMCFC12 = 120.914_JPRB
 ! * RMHCFC12     : HCFC22 molar mass
-REAL(KIND=JPRB), PARAMETER :: RMHCFC22 = 0.0_JPRB ! For now, ask Robin
+REAL(KIND=JPRB), PARAMETER :: RMHCFC22 = 86.469_JPRB
 ! * RMCCL4       : CCl4 molar mass
-REAL(KIND=JPRB), PARAMETER :: RMCCL4 = 0.0_JPRB ! For now, ask Robin
-
-!rcccl4 rcfc11 rccfc12 rch4 rcco2 rccfc22 rcn2o rcno2
+REAL(KIND=JPRB), PARAMETER :: RMCCL4 = 153.823_JPRB
 
 END MODULE YOMCST
