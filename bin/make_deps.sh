0e2b7d399f33e0f63b397c923d5781109add4baf
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2022-07-05 08:40:58 +0000

    Trim whitespace in make_deps

--- bin/make_deps.sh
+++ bin/make_deps.sh
@@ -14,7 +14,7 @@ EXT="parkind1.o|yomhook.o|yomcst.o|yomdyncore.o|yomlun.o|abor1.o|yomtag.o|mpl_mo
 
 while [ "$1" ]
 do
-    DEPS=$(egrep -i '^[ \t]*use' $1 | awk '-F[ ,]' '{print $2".o"}' | tr '[:upper:]' '[:lower:]' | egrep -v "$EXT" | tr '\n' ' ')
+    DEPS=$(egrep -i '^[ \t]*use' $1 | awk '{ sub(/^[ \t]+/, ""); print }' | awk '-F[ ,]' '{print $2".o"}' | tr '[:upper:]' '[:lower:]' | egrep -v "$EXT" | tr '\n' ' ')
     if [ "$DEPS" ]
     then
 	echo $1 | awk -F. '{print $1"'".o: $DEPS"'"}'
