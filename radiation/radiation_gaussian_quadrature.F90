c9e0365ff54e284d386ad9511c49c5a86472525f
Robin Hogan <r.j.hogan.ecmwf.int>
2022-11-04 09:49:59 +0000

    Added "Optimal" quadrature but needs more work

--- radiation/radiation_gaussian_quadrature.F90
+++ radiation/radiation_gaussian_quadrature.F90
@@ -23,15 +23,17 @@ module radiation_gaussian_quadrature
 
   enum, bind(c)
     enumerator IQuadratureLegendre, IQuadratureLaguerre, IQuadratureJacobi, &
-         &     IQuadratureElsasser, IQuadratureChandrasekhar, IQuadratureTransOpt
+         &     IQuadratureElsasser, IQuadratureChandrasekhar, IQuadratureTransOpt, &
+         &     IQuadratureOptimal
   end enum
 
-  character(len=*), parameter :: QuadratureName(0:5) = [ 'Legendre     ', &
+  character(len=*), parameter :: QuadratureName(0:6) = [ 'Legendre     ', &
        &                                                 'Laguerre     ', &
        &                                                 'Jacobi       ', &
        &                                                 'Elsasser     ', &
        &                                                 'Chandrasekhar', &
-       &                                                 'TransOpt     ' ]
+       &                                                 'TransOpt     ', &
+       &                                                 'Optimal      ' ]
   
 contains
   
@@ -126,15 +128,15 @@ contains
       ! Optimized values to minimize the error in transmission over a
       ! wide range of paths
       if (norder == 1) then
-        !nodes(1) = 1.0_jprb / 1.6145_jprb
-        !weights(1) = 0.5_jprb / nodes(1)
-        nodes(1) = 0.619392_jprb
-        weights(1) = 0.807244_jprb
+        nodes(1) = 1.0_jprb / 1.6145_jprb
+        weights(1) = 0.5_jprb / nodes(1)
+        !nodes(1) = 0.619392_jprb
+        !weights(1) = 0.807244_jprb
       else if (norder == 2) then
-        !nodes(1:2)   = [ 0.2599_jprb, 0.7797_jprb ]
-        !weights(1:2) = [ 0.42894_jprb, 0.49829_jprb ]
-        nodes(1:2) = [0.225988_jprb, 0.756382_jprb]
-        weights(1:2) = [0.399858_jprb, 0.541575_jprb]
+        nodes(1:2)   = [ 0.2599_jprb, 0.7797_jprb ]
+        weights(1:2) = [ 0.42894_jprb, 0.49829_jprb ]
+        !nodes(1:2) = [0.225988_jprb, 0.756382_jprb]
+        !weights(1:2) = [0.399858_jprb, 0.541575_jprb]
       else if (norder == 3) then
         nodes(1:3) = [0.0906266_jprb, 0.363077_jprb, 0.817142_jprb]
         weights(1:3) = [0.162957_jprb, 0.39496_jprb, 0.418325_jprb]
@@ -163,7 +165,37 @@ contains
         write(nulerr,'(a)') '*** Error: Transmission-optimized quadrature requires norder <= 8'
         call radiation_abort()
       end if
-      
+    else if (quadrature_type == IQuadratureOptimal) then
+      ! Optimized values to minimize the error in fluxes and heating
+      ! rates for the MLS standard atmosphere
+      if (norder == 1) then
+        nodes(1:1) = [0.606205]
+        weights(1:1) = [0.824804]
+      else if (norder == 2) then
+        nodes(1:2) = [0.189901, 0.738542]
+        weights(1:2) = [0.383466, 0.578409]
+      else if (norder == 3) then
+        nodes(1:3) = [0.0783442, 0.373655, 0.832172]
+        weights(1:3) = [0.170496, 0.424737, 0.394074]
+      else if (norder == 4) then
+        nodes(1:4) = [0.0366585, 0.178189, 0.490449, 0.872313]
+        weights(1:4) = [0.0820073, 0.219265, 0.391545, 0.304811]
+      else if (norder == 5) then
+        nodes(1:5) = [0.0255436, 0.101581, 0.268106, 0.56644, 0.893844]
+        weights(1:5) = [0.0542268, 0.109763, 0.232149, 0.349368, 0.254327]
+      else if (norder == 6) then
+        nodes(1:6) = [0.0251896, 0.0978779, 0.251806, 0.498906, 0.681858, 0.908207]
+        weights(1:6) = [0.053093, 0.103407, 0.210043, 0.24434, 0.174694, 0.214303]
+      else if (norder == 7) then
+        nodes(1:7) = [0.0246248, 0.0912663, 0.224044, 0.428732, 0.617081, 0.762264, 0.932376]
+        weights(1:7) = [0.0511669, 0.0917827, 0.176662, 0.212719, 0.160546, 0.151536, 0.155521]
+      else if (norder == 8) then
+        nodes(1:8) = [0.0249335, 0.094519, 0.234396, 0.42446, 0.544502, 0.67376, 0.814058, 0.935843]
+        weights(1:8) = [0.0522084, 0.0969639, 0.184164, 0.149288, 0.13727, 0.125729, 0.118452, 0.135833]
+      else
+        write(nulerr,'(a)') '*** Error: Optimized quadrature requires norder <= 8'
+        call radiation_abort()
+      end if
     end if
     
   end subroutine gaussian_quadrature
