e841ee3bc782473c841db850b599a524375b58c2
Paul Mullowney <Paul.Mullowney@amd.com>
2025-09-17 04:41:19 -0500

    Removing this code. The code validates in double precision against previously generated results for master-acc for ecrad_dp, ecrad_ifs_dp, and ecrad_ifs_blocked_dp

--- driver/ecrad_driver_read_input.F90
+++ driver/ecrad_driver_read_input.F90
@@ -192,16 +192,6 @@ contains
       cloud%re_ice => cloud%effective_radius(:,:,2)
       cloud%ntype = size(cloud%mixing_ratio,3)
 
-#ifdef DEBUG_WARNING
-      write(nulout,'(a,a,a,i0,a)') "    ", __FILE__, " : LINE = ", __LINE__, " has OpenACC code, acc_attach, that is not ported. Not sure what to do with this yet."
-#endif
-#ifdef _OPENACC
-      call acc_attach(cloud%q_liq)
-      call acc_attach(cloud%q_ice)
-      call acc_attach(cloud%re_liq)
-      call acc_attach(cloud%re_ice)
-#endif
-
       ! Simple initialization of the seeds for the Monte Carlo scheme
       call single_level%init_seed_simple(single_level,1,ncol,lacc=.false.)
       ! Overwrite with user-specified values if available
