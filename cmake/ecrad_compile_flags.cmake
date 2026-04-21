d05e52da4e55f70df1c069b69d06ee60e71bee7a
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2026-04-21 23:04:03 +0200

    Remove project overrides (will be provided by ecbuild)

--- cmake/ecrad_compile_flags.cmake
+++ cmake/ecrad_compile_flags.cmake
@@ -6,11 +6,6 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-# Capture ecbuild defaults and/or flags set by a toolchain
-set( ${PNAME}_Fortran_FLAGS "${${PNAME}_Fortran_FLAGS} ${ECBUILD_Fortran_FLAGS}" )
-set( ${PNAME}_Fortran_FLAGS_BIT "${${PNAME}_Fortran_FLAGS_BIT} ${ECBUILD_Fortran_FLAGS_BIT}" )
-set( ${PNAME}_Fortran_FLAGS_DEBUG "${${PNAME}_Fortran_FLAGS_DEBUG} ${ECBUILD_Fortran_FLAGS_DEBUG}" )
-
 if(CMAKE_Fortran_COMPILER_ID MATCHES "Cray")
   set(checkbounds_flags   "-Rb")
   set(fpe_flags           "-Ktrap=fp")
